eff049e106784d2ef9cd2e71d08075f7214073ed
DimuthuRooverr <dimuthu@rooverr.com>
2024-08-21 19:51:54 +0530

    implemented service file

---
+++ lib/services/networks/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:european_countries/model/country_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: "https://restcountries.com/v3.1")
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET("/region/europe?fields=name,capital,flags,region,languages,population")
+  Future<List<Country>> getEuropeanCountries();
+}
